4a5b5a97d87c2bac94f7547ac3a4dbd7b84420b2
Mikel Landa <mikel.landa@pepperservicing.es>
2023-06-30 10:12:11 +0200

    Add asdf script

--- .chezmoiscripts/run_onchange_before_asdf.sh
+++ .chezmoiscripts/run_onchange_before_asdf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 GREEN="\n\033[0;32m"
 RED='\n\033[0;31m'
 NC="\033[0m"
@@ -7,12 +9,24 @@ NC="\033[0m"
 function green() {
 	echo -e "${GREEN}$1${NC}"
 }
+
+function asdf_install() {
+	asdf plugin-add $@ && status_code=$? || status_code=$?
+	if [ $status_code -eq 0 ] || [ $status_code -eq 2 ]; then
+		green "$1 is installed"
+	fi
+}
 # Install asdf
 
 green "Installing asdf..."
 [ -d ~/.asdf ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 
-
 ## HASHICORP
-asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git || true
-asdf plugin-add packer https://github.com/asdf-community/asdf-hashicorp.git || true
+asdf_install terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf_install packer https://github.com/asdf-community/asdf-hashicorp.git
+
+asdf install terraform latest
+asdf install packer latest
+
+asdf global terraform latest
+asdf global packer latest
