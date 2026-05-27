f265c118cb8c6cc19d5a3a791394557c0cc064ac
Mikel Landa <mikel923@hotmail.com>
2026-05-27 18:31:52 +0200

    fix ssh

--- dot_config/zsh/plugins/gpg-agent.plugin.zsh
+++ dot_config/zsh/plugins/gpg-agent.plugin.zsh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
 
 load_gpg_agent() {
-  unset SSH_AGENT_PID
-  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
   gpg-connect-agent updatestartuptty /bye >/dev/null
 }
 
